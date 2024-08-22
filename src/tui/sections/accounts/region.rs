b806dff8a524b9cdf7309bf7b83c61f34c40d29f
PeterAkaJohn <pierpaolo.iannone93@gmail.com>
2024-08-22 20:38:00 +0200

    ref(keyevents): new handling in accounts/region

--- src/tui/sections/accounts/region.rs
+++ src/tui/sections/accounts/region.rs
@@ -1,3 +1,4 @@
+use crossterm::event::KeyModifiers;
 use ratatui::{
     layout::{Constraint, Direction, Layout},
     widgets::Clear,
@@ -6,10 +7,13 @@ use tokio::sync::mpsc::UnboundedSender;
 
 use crate::{
     action::Action,
-    tui::components::{
-        input::InputBlock,
-        popup::WithPopup,
-        traits::{Component, ComponentProps, WithContainer},
+    tui::{
+        components::{
+            input::InputBlock,
+            popup::WithPopup,
+            traits::{Component, ComponentProps, WithContainer},
+        },
+        key_event::{EventListeners, ExecuteEventListener, S3liKeyEvent, S3liOnChangeEvent},
     },
 };
 
@@ -18,6 +22,7 @@ pub struct Region {
     pub region: String,
     pub new_region: String,
     ui_tx: UnboundedSender<Action>,
+    listeners: Vec<EventListeners<Self>>,
 }
 
 impl Region {
@@ -27,7 +32,46 @@ impl Region {
             open: false,
             region: region.clone(),
             new_region: region.clone(),
+            listeners: Self::register_listeners(),
         }
+    }
+
+    fn register_listeners() -> Vec<EventListeners<Self>> {
+        vec![
+            EventListeners::KeyEvent((
+                S3liKeyEvent::new(vec![(crossterm::event::KeyCode::Esc, KeyModifiers::NONE)]),
+                Self::exit_component,
+            )),
+            EventListeners::KeyEvent((
+                S3liKeyEvent::new(vec![(crossterm::event::KeyCode::Enter, KeyModifiers::NONE)]),
+                Self::confirm,
+            )),
+            EventListeners::OnChangeEvent((
+                S3liOnChangeEvent::new(),
+                Self::add_char,
+                Self::delete_char,
+            )),
+        ]
+    }
+
+    fn exit_component(&mut self) {
+        self.open = false;
+        if !self.new_region.eq(&self.region) {
+            self.new_region.clone_from(&self.region);
+        }
+    }
+    fn confirm(&mut self) {
+        // send region to state with ui_tx
+        let _ = self
+            .ui_tx
+            .send(Action::ChangeRegion(self.new_region.clone()));
+        self.open = false;
+    }
+    fn delete_char(&mut self) {
+        self.new_region.pop();
+    }
+    fn add_char(&mut self, value: char) {
+        self.new_region.push(value);
     }
 }
 
@@ -42,6 +86,12 @@ impl WithPopup for Region {
 }
 
 impl WithContainer<'_> for Region {}
+
+impl ExecuteEventListener for Region {
+    fn get_event_listeners(&self) -> &Vec<EventListeners<Self>> {
+        &self.listeners
+    }
+}
 
 impl Component for Region {
     fn render(
@@ -68,28 +118,6 @@ impl Component for Region {
         f.render_widget(input, center_section);
     }
     fn handle_key_events(&mut self, key: crossterm::event::KeyEvent) {
-        match key.code {
-            crossterm::event::KeyCode::Esc => {
-                self.open = false;
-                if !self.new_region.eq(&self.region) {
-                    self.new_region.clone_from(&self.region);
-                }
-            }
-            crossterm::event::KeyCode::Enter => {
-                // send region to state with ui_tx
-                let _ = self
-                    .ui_tx
-                    .send(Action::ChangeRegion(self.new_region.clone()));
-                self.open = false;
-            }
-            crossterm::event::KeyCode::Backspace => {
-                // send region to state with ui_tx
-                self.new_region.pop();
-            }
-            crossterm::event::KeyCode::Char(value) => {
-                self.new_region.push(value);
-            }
-            _ => {}
-        }
+        self.execute(key);
     }
 }
